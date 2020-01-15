08c967a4cad1a8ffcc4c81862e16257787e8a728
Chloe Gell <gellert.chloe@gmail.com>
2020-01-15 20:44:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,11 @@
 # code your #valid_move? method here
-def valid_move?(board, index)
-  if((position_taken(board, index) == false) && index.between(0, 8))
-    
+def valid_move?(board, index, char)
+  if((position_taken(board, index) == false) && index.between?(0, 8))
+    board[index] == char
+  else 
+    puts "I'm sorry, that space is taken."
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
